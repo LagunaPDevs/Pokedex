5ef86a37cb307397b3177f05728569ab1c47eb88
Maria Laguna <marialagunap@gmail.com>
2024-10-17 07:48:03 +0200

    first commit

---
+++ mobile-app/lib/theme/color_constants.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+const primary = Color(0xffcc0000);
+
+// pokemon type colors
+const bugColor = Color(0xff94BC4A);
+const darkColor = Color(0xff736C75);
+const dragonColor = Color(0xff6A7BAF);
+const electricColor = Color(0xffE5C531);
+const fairyColor = Color(0xffE397D1);
+const fightingColor = Color(0xffCB5F48);
+const fireColor = Color(0xffEA7A3C);
+const flyingColor = Color(0xff7DA6DE);
+const ghostColor = Color(0xff846AB6);
+const grassColor = Color(0xff71C558);
+const groundColor = Color(0xffCC9F4F);
+const iceColor = Color(0xff70CBD4);
+const normalColor = Color(0xffAAB09F);
+const psychicColor = Color(0xffE5709B);
+const steelColor = Color(0xff89A1B0);
+const waterColor = Color(0xff539AE2);
+
+// greyscale
+const darkGreyColor = Color(0xff212121);
+const mediumGreyColor = Color(0xff666666);
+const lightGreyColor = Color(0xffE0E0E0);
+const backgroundGreyColor = Color(0xffEFEFEF);
+const whiteColor = Color(0xffffffff);
